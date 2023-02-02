d4eb61dc654c8c61ce122fb0ea07b911043c6c39
adrianwtam <adrianwtam@users.noreply.github.com>
2023-02-02 20:25:07 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,10 @@
+moreaEventData = [
+  {title: "E02: Quantum Physics Introduction", url: "/MoreaTest/morea/example-introduction/experience-introduce-yourself.html", start: "2022-07-08T23:00"},
+  {title: "E01: Differential Equation Basics", url: "/MoreaTest/morea/example-introduction/experience-environment-configuration.html", start: "2021-07-07T23:00"},
+  {title: "Introduction to MATH 483", url: "/MoreaTest/modules/example-introduction", start: "2023-01-14", end: "2023-01-29T23:00"},
+  {title: "A01: Quantum Coding in Python", url: "/MoreaTest/morea/example-ethics/assessment-ethics.html", start: "2021-07-30T09:00"},
+  {title: "E5: The Franck-Hertz Experiment", url: "/MoreaTest/morea/example-ethics/experience-se-ethics-harm.html", start: "2021-07-28T23:00"},
+  {title: "Quantum Physics Intro", url: "/MoreaTest/modules/ethics", start: "2023-01-31", end: "2023-02-14T23:00"},
+  {title: "E03: What is the Photoelectric Effect?", url: "/MoreaTest/morea/example-ethics/experience-se-ethics-codes.html", start: "2023-07-26T23:00"},
+  {title: "E04: Photons and Compton Scattering", url: "/MoreaTest/morea/example-ethics/experience-se-ethics-good.html", start: "2023-07-27T23:00"}
+]
